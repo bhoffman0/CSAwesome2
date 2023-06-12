ff4d6b266f4448e15128547fe8112f87f933424c
Peter Seibel <peter@gigamonkeys.com>
2023-06-12 15:13:43 -0700

    One more try to get this regex right.

--- fix-ptx.pl
+++ fix-ptx.pl
@@ -13,9 +13,9 @@ $^I = '';
 while (<>) {
 
   # Translate xml:ids that were generated in the docs to the ones used in the xrefs.
-  s/xml:id="_briceida-mariscal-id1-\d+"/xml:id="interviewees_b_-mariscal"/g;
-  s/xml:id="_carla-de-lira-id1-\d+"/xml:id="interviewees_c_-de_-lira"/g;
-  s/xml:id="_camille-mbayo-id1-\d+"/xml:id="interviewees_c_-mbayo"/g;
+  s/xml:id="_briceida-mariscal-id1(-\d+)?"/xml:id="interviewees_b_-mariscal"/g;
+  s/xml:id="_carla-de-lira-id1(-\d+)?"/xml:id="interviewees_c_-de_-lira"/g;
+  s/xml:id="_camille-mbayo-id1(-\d+)?"/xml:id="interviewees_c_-mbayo"/g;
   
   print;
 }
