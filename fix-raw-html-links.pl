a30b51c8cf76294c82991072d2a2dc52caf88482
Peter Seibel <peter@gigamonkeys.com>
2024-06-19 14:10:34 -0700

    Quiet down about the links.

--- fix-raw-html-links.pl
+++ fix-raw-html-links.pl
@@ -7,6 +7,8 @@
 use warnings;
 use strict;
 
+my $DEBUG = 0;
+
 $/ = undef;
 $^I = '';
 
@@ -17,7 +19,9 @@ while (<>) {
 
   # Find all the raw:: html links and stash them away
   while (/^\.\.\s+\|([^\|]+?)\|[ \t]+raw::[ \t]+html\s+<a href\s*=\s*"(.*?)".*?>(.*?)<\/a>\s+/msg) {
-    print STDERR "Found link $1.\n";
+    if ($DEBUG) {
+      print STDERR "Found link $1.\n";
+    }
     $urls{$1} = $2;
     $texts{$1} = $3;
   }
@@ -27,7 +31,9 @@ while (<>) {
 
   # And now replace the references to the raw:: html sections
   foreach my $slug (keys %urls) {
-    print STDERR "Replacing ", $slug, " in $ARGV\n";
+    if ($DEBUG) {
+      print STDERR "Replacing ", $slug, " in $ARGV\n";
+    }
     s{\|$slug\|}{'`' . $texts{$slug} . ' <' . $urls{$slug} . '>`_'}ge;
   }
 
