44a84a05c50a050c7f209625cc584bd798a809e7
Beryl Hoffman <hoffmanb@elms.edu>
2024-12-21 12:39:51 -0500

    Latest generated pretext

--- fix-data-stdin.pl
+++ fix-data-stdin.pl
@@ -16,9 +16,9 @@ if ($ARGV[0] eq "--debug") {
 $/ = undef;
 
 while (<>) {
-   s{>\s*<code>\s*import java\.awt}{ datafile="turtleClasses.jar,turtleClasses2.jar">\n\t<code>\n\timport java\.awt}msg;
+   s{>\s*<code>\s*import java\.awt}{ datafile="turtleClasses.jar,turtleClasses2.jar">\n<code>\nimport java\.awt}msg;
 
-   s{<code>\s*import java\.util.Scanner(.*?)</code>}{<code>\n\timport java\.util.Scanner$1</code>\n\t<stdin> </stdin>}msg;
+   s{<code>\s*import java\.util.Scanner(.*?)</code>}{<code>\nimport java\.util.Scanner$1</code>\n\t<stdin> </stdin>}msg;
 
 
   print;
