1bf3b492e152a036b7c7224008c3a2c40edd7702
Peter Seibel <peter@gigamonkeys.com>
2025-05-31 19:56:36 -0700

    Tweak to formatter for spaces between inline elements.

--- test-idempotency.sh
+++ test-idempotency.sh
@@ -3,10 +3,13 @@
 set -euo pipefail
 
 ./format-ptx.py "$1" > first.xml
-./format-ptx.py first.xml > second.xml
+./format-ptx.py -q first.xml > second.xml
 sum1=$(shasum first.xml | cut -c -40)
 sum2=$(shasum second.xml | cut -c -40)
 
 if [ "$sum1" != "$sum2" ]; then
     echo "$1"
+    exit 1
+else
+    exit 0
 fi
